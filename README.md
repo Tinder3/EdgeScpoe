e2235719747b39025d38741644b572759b14a890
Tinder3 <lxy740322@gmail.com>
2026-09-03 18:45:22 +0800

    build dir

---
+++ README.md
@@ -0,0 +1,12 @@
+# EdgeScope
+
+Linux 远程诊断系统。
+
+## 技术栈
+
+- C++17
+- CMake 3.16+
+
+## 开发状态
+
+最小工程骨架，agent/client 可编译运行。
---
+++ client/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "EdgeScope Client starting..." << std::endl;
+    return 0;
+}
